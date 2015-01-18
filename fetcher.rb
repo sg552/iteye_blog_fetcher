bcd0a841130ecbb6685f17e491ab36cf572afa9a
Siwei <shensiwei@sina.com>
2015-01-18 14:11:14 +0000

    实现了抓取单独的某篇文章

--- fetcher.rb
+++ fetcher.rb
@@ -12,16 +12,37 @@ class Fetcher
     'Accept-Language' => 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4'
   
   def self.fetch 
+    your_post_urls = []
     (1..MAX_PAGE).each do |index|
-      remote_result = Fetcher.get("#{BASE_URL}?page=#{index}")
-      puts "== result: #{remote_result}"
+      remote_result = Fetcher.get("#{BASE_URL}?page=#{index}").body
+      your_post_urls += self.extract_post_link remote_result
+    end
+    your_post_urls.each  do |url|
+      information = self.extract_post_information_from_single_page url
+      insert_as_local_posts(information)
     end
   end
 
   private
+  def self.insert_as_local_posts information
+     # FIXME insert to our local database
+     # Blog.create :content => information[:content], :title => information[:title], 
+     #   :created_at => information[:created_at]
+  end
   def self.extract_post_link content
-      return Nokogiri::HTML(content).css('.blog_title h3 a').map { |a|
-	a.attr('href') 
-      }
+    return Nokogiri::HTML(content).css('.blog_title h3 a').map { |a|
+      a.attr('href') 
+    }
   end 
+  def self.extract_post_information_from_single_page url
+    remote_result = Fetcher.get(url).body
+    result = {}
+    return {
+      :content => Nokogiri::HTML(remote_result).css('#blog_content').to_html, 
+      :title => Nokogiri::HTML(remote_result).css('.blog_title h3 a').text,
+      :created_at => Nokogiri::HTML(remote_result).css('.blog_bottom li')[0].text + ':00'
+    }
+  end
+  
 end
+
--- spec/fetcher_spec.rb
+++ spec/fetcher_spec.rb
@@ -9,10 +9,21 @@ describe Fetcher do
   </div>
   应该获得 title 的link:  /blog/89390
   ' do 
-    content = %Q{<div class="blog_title">
-    <h3><a href="/blog/89390">应用开源项目时，你会大肆封装，修改它吗？</a></h3>
-    <div class="news_tag"><a href="http://www.iteye.com/blogs/tag/%E4%BC%81%E4%B8%9A%E5%BA%94%E7%94%A8">企业应用</a><a href="http://www.iteye.com/blogs/tag/%E9%A1%B9%E7%9B%AE%E7%AE%A1%E7%90%86">项目管理</a><a href="http://www.iteye.com/blogs/tag/%E5%BA%94%E7%94%A8%E6%9C%8D%E5%8A%A1%E5%99%A8">应用服务器</a><a href="http://www.iteye.com/blogs/tag/Struts">Struts</a><a href="http://www.iteye.com/blogs/tag/Acegi">Acegi</a></div>
-  </div>}
+    content = %Q{
+	  <div class="blog_title">
+	    <h3><a href="/blog/89390">应用开源项目时，你会大肆封装，修改它吗？</a></h3>
+	    <div class="news_tag"><a href="http://www.iteye.com/blogs/tag/%E4%BC%81%E4%B8%9A%E5%BA%94%E7%94%A8">企业应用</a><a href="http://www.iteye.com/blogs/tag/%E9%A1%B9%E7%9B%AE%E7%AE%A1%E7%90%86">项目管理</a><a href="http://www.iteye.com/blogs/tag/%E5%BA%94%E7%94%A8%E6%9C%8D%E5%8A%A1%E5%99%A8">应用服务器</a><a href="http://www.iteye.com/blogs/tag/Struts">Struts</a><a href="http://www.iteye.com/blogs/tag/Acegi">Acegi</a></div>
+	  </div>
+    }
     Fetcher.send(:extract_post_link, content).should == ['/blog/89390']
   end 
+
+  it '给定某个POST的单独页面，需要抽取出文章标题, 正文，时间。,例如： http://sg552.iteye.com/blog/89390
+  ' do 
+    url = 'http://sg552.iteye.com/blog/89390'
+    information = Fetcher.send(:extract_post_information_from_single_page, url)
+    information[:title].should == '应用开源项目时，你会大肆封装，修改它吗？'
+    information[:content].size.should == 652
+    information[:created_at].should == '2007-06-12 08:52:00'
+  end
 end
