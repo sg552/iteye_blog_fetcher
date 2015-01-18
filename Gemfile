cf22476c1239e8686667e9e17081e69c26ecb12c
Siwei <shensiwei@sina.com>
2015-01-18 11:38:53 +0000

    initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+gem 'httparty', '0.13.3'
+gem 'nokogiri', '1.6.5'
+gem 'rspec', '2.9.0'
---
+++ fetcher.rb
@@ -0,0 +1,24 @@
+# -*- encoding : utf-8 -*-
+require 'nokogiri'
+require 'httparty'
+
+class Fetcher
+  include HTTParty
+  BASE_URL = 'http://sg552.iteye.com'
+  MAX_PAGE = 1
+  headers 'User-Agent' => 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+    'Referer' => BASE_URL,
+    'Host' => 'sg552.iteye.com',
+    'Accept-Language' => 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4'
+  
+  def self.fetch 
+    (1..MAX_PAGE).each do |index|
+      remote_result = Fetcher.get("#{BASE_URL}?page=#{index}")
+      puts "== result: #{remote_result}"
+      doc = Nokogiri::HTML(remote_result)
+      doc.css('.blog_title h3 a').map { |a|
+	puts  a.attr('href') 
+      }
+    end
+  end
+end
