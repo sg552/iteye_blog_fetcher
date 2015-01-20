a103aa3504ce8d35693395325f535b1b8a52d526
Siwei <shensiwei@sina.com>
2015-01-20 11:00:04 +0800

    增加了README

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-# iteye_blog_fetcher
-用来抓取iteye网站的 博客的脚本.   
+# Blog Fetcher
 
-自从12年有了自己的主机之后，就一直想把在javaeye (iteye) 上的博客同步过来。可惜一直没能做成。今天就做一下吧。  spec: 单元测试。 运行方式： 需要把它弄到自己的rails blog项目中，或者直接写到 ActiveRecord 对应的table中？ 还没想好。
+用来抓取iteye网站的 博客的脚本.
+
+自从12年有了自己的主机之后，就一直想把在javaeye (iteye) 上的博客同步过来。可惜一直没能做成。今天就做一下吧。
+
+# 原理
+
+只适用于抓取  base_url?page=1  ,  base_url?page=2 这样类型的博客。
+
+人肉写好 要抓取的页面的URL， 以及总共有多少个页面，就可以抓取了。
+
+# 安装
+
+$ git clone ...
+
+$ cd <project_path>
+
+$ bundle install
+
+修改 `fetcher.rb`  ， 文件，把需要抓取的地址和页数写好。
+
+修改 config/database.yml 文件，按照你的数据库配置来。
+
+# 运行
+
+$ ruby run.rb
+
+# 测试：
+
+$ bundle exec rspec spec
+
+# 我的个人博客
+
+http://siwei.me  有问题欢迎联系我
--- fetcher.rb
+++ fetcher.rb
@@ -10,14 +10,13 @@ class Fetcher
     'Referer' => BASE_URL,
     'Host' => 'sg552.iteye.com',
     'Accept-Language' => 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4'
-  
-  def self.fetch 
+
+  def self.fetch
     your_post_urls = []
     (1..MAX_PAGE).each do |index|
       remote_result = Fetcher.get("#{BASE_URL}?page=#{index}").body
       your_post_urls += self.extract_post_link remote_result
     end
-puts "== your_post_urls: #{your_post_urls.inspect}"
     your_post_urls.each  do |url|
       information = self.extract_post_information_from_single_page url
       insert_as_local_posts(information)
@@ -33,18 +32,18 @@ puts "== your_post_urls: #{your_post_urls.inspect}"
   end
   def self.extract_post_link content
     return Nokogiri::HTML(content).css('.blog_title h3 a').map { |a|
-      a.attr('href') 
+      a.attr('href')
     }
-  end 
+  end
   def self.extract_post_information_from_single_page url
     remote_result = Fetcher.get(url).body
     result = {}
     return {
-      :content => Nokogiri::HTML(remote_result).css('#blog_content').to_html, 
+      :content => Nokogiri::HTML(remote_result).css('#blog_content').to_html,
       :title => Nokogiri::HTML(remote_result).css('.blog_title h3 a').text,
       :created_at => Nokogiri::HTML(remote_result).css('.blog_bottom li')[0].text + ':00'
     }
   end
-  
+
 end
 
